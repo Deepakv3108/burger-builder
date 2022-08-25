3e0d4333ec8163d6df0aba07a856df1464cfb1f2
Deepak Vishwakarma <deepak@logicloop.io>
2022-08-25 15:17:24 +0530

    Button Click Evenet Handler added and Dynamically passed data from one Component to another

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
+import { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I am React App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: [
+      {name: "Deepak", age:28},
+      {name: "Malatidevi", age:52},
+      {name: "Deepa", age:27}
+    ]
+  }
+
+  switchNamehandler = (newName) => {
+    this.setState({
+      person: [
+        {name: newName, age:28},
+        {name: "Malatidevi", age:50},
+        {name: "Deepa1", age:27}
+      ]
+    });
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <h1>Hi, I am React App</h1>
+        <p>It is working</p>
+        <button onClick={() => this.switchNamehandler("Vishwakarma")}>Switch Name</button>
+        <Person 
+          name={this.state.person[0].name} 
+          age={this.state.person[0].age}
+          />
+        <Person 
+          name={this.state.person[1].name} 
+          age={this.state.person[1].age}
+          click={this.switchNamehandler.bind(this, "Bhoju")}
+          >Hobby: Watch Over Family</Person>
+        <Person 
+          name={this.state.person[2].name} 
+          age={this.state.person[2].age}
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
